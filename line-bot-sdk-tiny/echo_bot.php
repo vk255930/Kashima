2ec90ebc3988d4ef74ae091f3ca1158a5f4465dd
vk255930 <crettchen@gmail.com>
2017-03-12 00:51:10 +0800

    New respond name

--- line-bot-sdk-tiny/echo_bot.php
+++ line-bot-sdk-tiny/echo_bot.php
@@ -33,7 +33,7 @@ foreach ($client->parseEvents() as $event) {
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $message['text']
+                                'text' => '主人, '.$message['text']
                             )
                         )
                     ));
